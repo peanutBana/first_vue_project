e43540b9e9743998989d3f733c8f8866eab2ada5
peanutBana <diao0321@gmail.com>
2023-10-16 23:01:35 +0900

    app.js 추가

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
+var cmp = {
+	template: '<p>This is another local child component</p>'
+};
+
+//실습 1
+Vue.component('todo-footer',{
+        template: '<p>This is another global child component</p>'
+    })
+
 var app = new Vue({
     el : '#app',
     data: {
         message: 'This is a parent component'
-    }
+    },
+    //실습 2
+    components: {
+		'todo-list': cmp
+	}
 })
